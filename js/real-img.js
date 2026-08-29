fb6e5275efc461aa44795bfbb904abe8513b70c4
aejabri <aejabri@gmail.com>
2026-08-30 00:05:18 +0300

    Point 3D tab at cutaway loader script

--- js/real-img.js
+++ js/real-img.js
@@ -1,6 +1,4 @@
 (function(){
-  var p=window.BODY_REAL_PARTS||[];
-  window.BODY_REAL_IMG='data:image/jpeg;base64,'+p.join('');
   var i=document.getElementById('realbody');
-  if(i) i.src=window.BODY_REAL_IMG;
+  if(i && window.BODY_REAL_IMG) i.src=window.BODY_REAL_IMG;
 })();
